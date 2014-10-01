6e7ac2c7857fe192987784d2723d35b9426d95f0
Marc Cirauqui <marc.cirauqui@abiquo.com>
2014-10-01 23:43:02 +0200

    typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -302,7 +302,7 @@ class anycloud (
 
   $curr_array = path_explode("$deploy_root/current")
   $conf_array = path_explode("$deploy_root/config")
-  $diff = difference($curr_array, conf_array)
+  $diff = difference($curr_array, $conf_array)
   $path_array = concat($conf_array, $diff)
   file { $path_array:
     ensure  => directory,
